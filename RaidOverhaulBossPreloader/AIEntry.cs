c4713090c92b0bdeb61ef2bf83bdd2df407410f4
DJ <147062692+KillerDJLang@users.noreply.github.com>
2024-04-12 23:01:04 -0400

    Add files via upload

---
+++ RaidOverhaulBossPreloader/AIEntry.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace LegionPrePatch
+{
+    [BepInPlugin("DJ.LegionPrePatch", "Legion Boss PrePatch", "1.0.0")]
+    public class AIEntry : BaseUnityPlugin
+    {
+        public static AIEntry Instance { get; private set; }
+
+        public void Awake()
+        {
+            AIEntry.Instance = this;
+        }
+    }
+}
