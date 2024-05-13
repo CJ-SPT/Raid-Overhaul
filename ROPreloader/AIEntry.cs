9ddb6c10638d721cdf861e775a87a631c81e5b9b
DJ <147062692+KillerDJLang@users.noreply.github.com>
2024-05-12 20:56:47 -0400

    Add files via upload

--- ROPreloader/AIEntry.cs
+++ ROPreloader/AIEntry.cs
@@ -2,7 +2,7 @@
 
 namespace LegionPrePatch
 {
-    [BepInPlugin("DJ.LegionPrePatch", "Legion Boss PrePatch", "1.1.0")]
+    [BepInPlugin("DJ.LegionPrePatch", "Legion Boss PrePatch", "2.2.0")]
     public class LegionAIEntry : BaseUnityPlugin
     {
         public static LegionAIEntry Instance { get; private set; }
