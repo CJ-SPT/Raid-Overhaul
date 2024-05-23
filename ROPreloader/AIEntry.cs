3f85059b35aa870cf2884225e6dc1415bd98e39e
DJ <147062692+KillerDJLang@users.noreply.github.com>
2024-05-22 23:55:33 -0400

    Update AIEntry.cs

--- ROPreloader/AIEntry.cs
+++ ROPreloader/AIEntry.cs
@@ -2,7 +2,7 @@
 
 namespace LegionPrePatch
 {
-    [BepInPlugin("DJ.LegionPrePatch", "Legion Boss PrePatch", "2.2.0")]
+    [BepInPlugin("DJ.LegionPrePatch", "Legion Boss PrePatch", "2.2.1")]
     public class LegionAIEntry : BaseUnityPlugin
     {
         public static LegionAIEntry Instance { get; private set; }
